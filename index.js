4a79cb43e2daa177ceb91a4fa10f7a89e953a928
Wilbert Tanoto <wilbert.tanoto@webimp.com.sg>
2019-09-29 23:36:20 +0700

    Add button to the index.html to retrieve current location

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public')); 
+app.use(express.json());
+
+const allData = [];
+
+// POST method route
+app.post('/api', function (request, response) {
+    const data = request.body;
+    allData.push(data);
+    response.json(allData);
+    console.log(allData);
+});
