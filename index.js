6b44c9889df424de637db0bd5ee4eff489617eb6
Wilbert Tanoto <wilbert.tanoto@webimp.com.sg>
2019-09-29 23:55:26 +0700

    Using nedb as small db

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public')); 
 app.use(express.json());
 
-const allData = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 // POST method route
 app.post('/api', function (request, response) {
     const data = request.body;
-    allData.push(data);
-    response.json(allData);
-    console.log(allData);
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+
+    database.insert(data);
+    response.json({
+        status: 'success',
+        timestamp: timestamp,
+        lat: data.lat,
+        long: data.lng,
+    })
 });
